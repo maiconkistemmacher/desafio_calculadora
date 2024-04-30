377c051b778881065030064402f776b2fff97dd0
Maicon Kistemmacher <maicon.kistemmacher@gmail.com>
2024-04-30 05:00:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-// TO-DO 
-// Fazer a funcionalidade de calcular
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('button');
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var valor = this.value;
+            var input = document.querySelector('input').value;
+            if (valor === 'limpar') {
+                document.querySelector('input').value = '';
+            } else if (valor === '=') {
+                document.querySelector('input').value = eval(input);
+            } else {
+                document.querySelector('input').value = input + valor;
+            }
+        });
+    });
+});
